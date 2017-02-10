bcf58fcc3f1ceb2acc94410427e4a64c7487bd57
Gianlu <altomanigianluca@gmail.com>
2017-02-10 18:52:56 +0100

    Date range indicator

--- app/src/main/java/com/gianlu/timeless/Activities/ProjectsActivity.java
+++ app/src/main/java/com/gianlu/timeless/Activities/ProjectsActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Pair;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.borax12.materialdaterangepicker.date.DatePickerDialog;
 import com.gianlu.commonutils.CommonUtils;
@@ -19,10 +20,12 @@ import com.gianlu.timeless.Objects.Project;
 import com.gianlu.timeless.R;
 import com.gianlu.timeless.Utils;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ProjectsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private Pair<Date, Date> currentRange;
@@ -39,6 +42,7 @@ public class ProjectsActivity extends AppCompatActivity implements DatePickerDia
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.setDisplayHomeAsUpEnabled(true);
+
         pager = (ViewPager) findViewById(R.id.projects_pager);
         pager.setOffscreenPageLimit(4);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.projects_tabs);
@@ -87,6 +91,22 @@ public class ProjectsActivity extends AppCompatActivity implements DatePickerDia
                 onBackPressed();
             }
         });
+
+        updateRangeText();
+    }
+
+    public void updateRangeText() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMM", Locale.getDefault());
+                TextView rangeTextView = (TextView) findViewById(R.id.projects_rangeText);
+                if (currentRange.first.getTime() == currentRange.second.getTime())
+                    rangeTextView.setText(formatter.format(currentRange.first));
+                else
+                    rangeTextView.setText(formatter.format(currentRange.first) + " - " + formatter.format(currentRange.second));
+            }
+        });
     }
 
     @Override
@@ -115,12 +135,15 @@ public class ProjectsActivity extends AppCompatActivity implements DatePickerDia
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
         Calendar start = Calendar.getInstance();
+        start.setTimeInMillis(0);
         start.set(year, monthOfYear, dayOfMonth);
 
         Calendar end = Calendar.getInstance();
+        end.setTimeInMillis(0);
         end.set(yearEnd, monthOfYearEnd, dayOfMonthEnd);
 
         currentRange = new Pair<>(start.getTime(), end.getTime());
+        updateRangeText();
 
         final ProgressDialog pd = CommonUtils.fastIndeterminateProgressDialog(this, R.string.loadingData);
         CommonUtils.showDialog(this, pd);
@@ -134,7 +157,9 @@ public class ProjectsActivity extends AppCompatActivity implements DatePickerDia
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        int sel = pager.getCurrentItem();
                         pager.setAdapter(new PagerAdapter(getSupportFragmentManager(), fragments));
+                        pager.setCurrentItem(sel, false);
                         pd.dismiss();
                     }
                 });
