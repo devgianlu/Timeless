404c0d42f5f70c71e8a0ae53d54131d719741df6
Gianlu <altomanigianluca@gmail.com>
2017-02-10 18:11:00 +0100

    FUCK. JUST ONE FUCKING LINE OF CODE SOLVED THAT DAMN ISSUE AND NOW I HAVE WASTED 3 HOURS

--- app/src/main/java/com/gianlu/timeless/Activities/PagerAdapter.java
+++ app/src/main/java/com/gianlu/timeless/Activities/PagerAdapter.java
@@ -2,11 +2,11 @@ package com.gianlu.timeless.Activities;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 
 import java.util.List;
 
-class PagerAdapter extends FragmentPagerAdapter {
+class PagerAdapter extends FragmentStatePagerAdapter {
     private final List<Fragment> fragments;
 
     PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
