ba9f763f2c5f4b61afc19714510bba5ba40c4cad
Gianlu <altomanigianluca@gmail.com>
2017-02-15 21:47:14 +0100

    Multiple projects in DurationsView

--- app/src/main/java/com/gianlu/timeless/DurationsView.java
+++ app/src/main/java/com/gianlu/timeless/DurationsView.java
@@ -6,10 +6,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.gianlu.timeless.Objects.Duration;
 
@@ -20,110 +22,153 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class DurationsView extends View {
-    private Map<Long, Long> values;
-    private Rect textBounds = new Rect();
-    private Paint durationPaint;
-    private Paint gridPaint;
-    private Paint textPaint;
-    private float padding;
+public class DurationsView extends LinearLayout {
     private List<String> projects;
 
     public DurationsView(Context context) {
         super(context);
-        init(context);
+        setOrientation(VERTICAL);
     }
 
     public DurationsView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        setOrientation(VERTICAL);
     }
 
     public DurationsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        setOrientation(VERTICAL);
     }
 
-    public DurationsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
-    }
-
-    private void init(Context context) {
-        durationPaint = new Paint();
-        durationPaint.setColor(ContextCompat.getColor(context, R.color.colorAccent));
-        durationPaint.setAlpha(100);
-
-        gridPaint = new Paint();
-        gridPaint.setColor(Color.GRAY);
-
-        textPaint = new Paint();
-        textPaint.setColor(Color.BLACK);
-        textPaint.setAntiAlias(true);
-        textPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, context.getResources().getDisplayMetrics()));
-    }
-
-    @SuppressLint("UseSparseArrays")
     public void setDurations(List<Duration> durations) {
-        values = new HashMap<>();
-        projects = new ArrayList<>();
-        Calendar cal = Calendar.getInstance();
-
-        for (Duration duration : durations) {
-            cal.setTimeInMillis(duration.time * 1000);
-            cal.set(Calendar.HOUR_OF_DAY, 0);
-            cal.set(Calendar.MINUTE, 0);
-            cal.set(Calendar.SECOND, 0);
-            cal.set(Calendar.MILLISECOND, 0);
-
-            values.put(TimeUnit.MILLISECONDS.toSeconds((duration.time * 1000) - cal.getTimeInMillis()), duration.duration);
-
+        this.projects = new ArrayList<>();
+        for (Duration duration : durations)
             if (!projects.contains(duration.project))
                 projects.add(duration.project);
+
+        for (String project : projects)
+            addView(new ChartView(getContext(), project, Duration.filter(durations, project), projects.size() == 1));
+    }
+
+    private class ChartView extends View {
+        private final boolean lonely;
+        private final String project;
+        private final Map<Long, Long> values;
+        private final Rect textBounds = new Rect();
+        private final Rect titleTextBounds = new Rect();
+        private final Paint titleTextPaint;
+        private final Paint durationPaint;
+        private final Paint gridPaint;
+        private final Paint textPaint;
+        private final int padding;
+        private final int defaultHeight;
+        private float internalPadding;
+
+        @SuppressLint("UseSparseArrays")
+        public ChartView(Context context, String project, List<Duration> durations, boolean lonely) {
+            super(context);
+            durationPaint = new Paint();
+            durationPaint.setColor(ContextCompat.getColor(context, R.color.colorAccent));
+
+            gridPaint = new Paint();
+            gridPaint.setColor(Color.GRAY);
+
+            textPaint = new Paint();
+            textPaint.setColor(Color.BLACK);
+            textPaint.setAntiAlias(true);
+            textPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, context.getResources().getDisplayMetrics()));
+
+            titleTextPaint = new Paint();
+            titleTextPaint.setColor(Color.BLACK);
+            titleTextPaint.setAlpha(64);
+            titleTextPaint.setAntiAlias(true);
+            titleTextPaint.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Medium.ttf"));
+            titleTextPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 40, context.getResources().getDisplayMetrics()));
+
+            this.project = project;
+            this.lonely = lonely;
+            this.values = new HashMap<>();
+            defaultHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, context.getResources().getDisplayMetrics());
+            Calendar cal = Calendar.getInstance();
+
+            for (Duration duration : durations) {
+                cal.setTimeInMillis(duration.time * 1000);
+                cal.set(Calendar.HOUR_OF_DAY, 0);
+                cal.set(Calendar.MINUTE, 0);
+                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.MILLISECOND, 0);
+
+                values.put(TimeUnit.MILLISECONDS.toSeconds((duration.time * 1000) - cal.getTimeInMillis()), duration.duration);
+
+                if (!projects.contains(duration.project))
+                    projects.add(duration.project);
+            }
+
+            if (lonely)
+                padding = 0;
+            else
+                padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, context.getResources().getDisplayMetrics());
         }
 
-        invalidate();
-    }
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(defaultHeight, MeasureSpec.EXACTLY));
+        }
 
-    private void calcPadding() {
-        textPaint.getTextBounds("24", 0, 2, textBounds);
-        padding = textBounds.width() / 2;
-    }
+        private void calcInternalPadding() {
+            textPaint.getTextBounds("24", 0, 2, textBounds);
+            internalPadding = textBounds.width() / 2;
+        }
 
-    private void adjustTextSize(Canvas canvas) {
-        calcPadding();
-        float secPerPixel = ((float) canvas.getWidth() - (padding * 2)) / 86400f;
+        private void adjustTextSize(Canvas canvas) {
+            calcInternalPadding();
+            float secPerPixel = ((float) canvas.getWidth() - (internalPadding * 2)) / 86400f;
 
-        for (int i = 0; i <= 24; i++) {
-            String hour = String.valueOf(i);
-            textPaint.getTextBounds(hour, 0, hour.length(), textBounds);
+            for (int i = 0; i <= 24; i++) {
+                String hour = String.valueOf(i);
+                textPaint.getTextBounds(hour, 0, hour.length(), textBounds);
 
-            if (textBounds.width() >= 3600 * secPerPixel) {
-                textPaint.setTextSize(textPaint.getTextSize() - 1);
-                adjustTextSize(canvas);
-                break;
+                if (textBounds.width() >= 3600 * secPerPixel) {
+                    textPaint.setTextSize(textPaint.getTextSize() - 1);
+                    adjustTextSize(canvas);
+                    break;
+                }
             }
         }
-    }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        calcPadding();
-        float secPerPixel = ((float) canvas.getWidth() - (padding * 2)) / 86400f;
-        adjustTextSize(canvas);
-
-        for (int i = 0; i <= 24; i++) {
-            String hour = String.valueOf(i);
-            float pos = (i * 3600 * secPerPixel) + padding;
-
-            textPaint.getTextBounds(hour, 0, hour.length(), textBounds);
-            canvas.drawLine(pos, 0, pos, canvas.getHeight() - textBounds.height() - 5, gridPaint);
-            if (i % 2 == 0)
-                canvas.drawText(hour, pos - (textBounds.width() / 2), canvas.getHeight(), textPaint);
+        private void adjustTitleTextSize(Canvas canvas) {
+            titleTextPaint.getTextBounds(project, 0, project.length(), titleTextBounds);
+            if (titleTextBounds.width() >= canvas.getWidth()) {
+                titleTextPaint.setTextSize(titleTextPaint.getTextSize() - 1);
+                adjustTitleTextSize(canvas);
+            }
         }
 
-        if (values != null)
-            for (Map.Entry<Long, Long> entry : values.entrySet())
-                canvas.drawRect((entry.getKey() * secPerPixel) + padding, 0, ((entry.getKey() + entry.getValue()) * secPerPixel) + padding, canvas.getHeight() - textBounds.height() - 5, durationPaint);
+        @Override
+        protected void onDraw(Canvas canvas) {
+            calcInternalPadding();
+            float secPerPixel = ((float) canvas.getWidth() - (internalPadding * 2)) / 86400f;
+            adjustTextSize(canvas);
+
+            if (!lonely) {
+                adjustTitleTextSize(canvas);
+                titleTextPaint.getTextBounds(project, 0, project.length(), titleTextBounds);
+                canvas.drawText(project, (canvas.getWidth() - titleTextBounds.width()) / 2, ((canvas.getHeight() + titleTextBounds.height()) / 2) - textBounds.height() - 5, titleTextPaint);
+            }
+
+            for (int i = 0; i <= 24; i++) {
+                String hour = String.valueOf(i);
+                float pos = (i * 3600 * secPerPixel) + internalPadding;
+
+                textPaint.getTextBounds(hour, 0, hour.length(), textBounds);
+                canvas.drawLine(pos, padding, pos, canvas.getHeight() - textBounds.height() - 5 - padding, gridPaint);
+                if (i % 2 == 0)
+                    canvas.drawText(hour, pos - (textBounds.width() / 2), canvas.getHeight() - padding, textPaint);
+            }
+
+            if (values != null)
+                for (Map.Entry<Long, Long> entry : values.entrySet())
+                    canvas.drawRect((entry.getKey() * secPerPixel) + internalPadding, padding, ((entry.getKey() + entry.getValue()) * secPerPixel) + internalPadding, canvas.getHeight() - textBounds.height() - 5 - padding, durationPaint);
+        }
     }
 }
--- app/src/main/java/com/gianlu/timeless/Objects/Duration.java
+++ app/src/main/java/com/gianlu/timeless/Objects/Duration.java
@@ -3,6 +3,10 @@ package com.gianlu.timeless.Objects;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Duration {
     public final long time;
     public final long duration;
@@ -12,5 +16,14 @@ public class Duration {
         time = obj.getLong("time");
         duration = obj.getLong("duration");
         project = obj.getString("project");
+    }
+
+    public static List<Duration> filter(List<Duration> items, String project) {
+        List<Duration> durations = new ArrayList<>();
+        for (Duration item : items)
+            if (Objects.equals(item.project, project))
+                durations.add(item);
+
+        return durations;
     }
 }
