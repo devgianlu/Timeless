93423ea96df0e4dea865af4d17a6add039dab64a
Gianlu <altomanigianluca@gmail.com>
2019-02-27 15:29:24 +0100

    Removed widget stuff + updated submodules

--- app/src/main/java/com/gianlu/timeless/PreferenceActivity.java
+++ app/src/main/java/com/gianlu/timeless/PreferenceActivity.java
@@ -38,7 +38,7 @@ public class PreferenceActivity extends BasePreferenceActivity {
     }
 
     @Override
-    protected boolean disablePayPalOnGooglePlay() {
+    protected boolean disableOtherDonationsOnGooglePlay() {
         return false;
     }
 
