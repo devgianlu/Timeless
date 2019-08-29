9ab84f3fd1824c08409bc13b76dd81a088f708e9
Gianlu <altomanigianluca@gmail.com>
2019-08-29 10:30:01 +0200

    Fixed GrantActivity

--- app/src/main/java/com/gianlu/timeless/GrantActivity.java
+++ app/src/main/java/com/gianlu/timeless/GrantActivity.java
@@ -3,9 +3,11 @@ package com.gianlu.timeless;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 
 import com.gianlu.commonutils.Dialogs.ActivityWithDialog;
 import com.gianlu.commonutils.Dialogs.DialogUtils;
@@ -30,6 +32,14 @@ public class GrantActivity extends ActivityWithDialog implements WakaTime.Initia
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grant);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         builder = new WakaTime.Builder(this);
 
