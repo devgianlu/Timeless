4659d10d8b57ce0ab485b7c54827505b34d062e0
Gianlu <altomanigianluca@gmail.com>
2021-05-02 15:07:01 +0200

    Do not hide navigation in GrantActivity (#44)

--- app/src/main/java/com/gianlu/timeless/GrantActivity.java
+++ app/src/main/java/com/gianlu/timeless/GrantActivity.java
@@ -44,9 +44,7 @@ public class GrantActivity extends ActivityWithDialog implements WakaTime.Initia
         if (actionBar != null)
             actionBar.hide();
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         builder = new WakaTime.Builder(this);
 
