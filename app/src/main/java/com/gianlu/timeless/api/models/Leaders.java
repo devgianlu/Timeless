26bf14d1a0114c11be19dc55263d49610b416766
devgianlu <altomanigianluca@gmail.com>
2021-12-22 19:41:45 +0100

    Catch errors while parsing leaders response

--- app/src/main/java/com/gianlu/timeless/api/models/Leaders.java
+++ app/src/main/java/com/gianlu/timeless/api/models/Leaders.java
@@ -1,5 +1,7 @@
 package com.gianlu.timeless.api.models;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,11 +9,18 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class Leaders extends ArrayList<Leader> {
+    private static final String TAG = Leaders.class.getSimpleName();
     public final int maxPages;
 
     public Leaders(JSONObject obj) throws JSONException {
         JSONArray array = obj.getJSONArray("data");
-        for (int i = 0; i < array.length(); i++) add(new Leader(array.getJSONObject(i)));
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                add(new Leader(array.getJSONObject(i)));
+            } catch (JSONException ex) {
+                Log.w(TAG, "Cannot parse leader at " + i, ex);
+            }
+        }
 
         maxPages = obj.getInt("total_pages");
     }
--- app/src/main/java/com/gianlu/timeless/api/models/LeadersWithMe.java
+++ app/src/main/java/com/gianlu/timeless/api/models/LeadersWithMe.java
@@ -1,15 +1,28 @@
 package com.gianlu.timeless.api.models;
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LeadersWithMe extends Leaders {
+    private static final String TAG = LeadersWithMe.class.getSimpleName();
     public final Leader me;
 
     public LeadersWithMe(JSONObject obj) throws JSONException {
         super(obj);
 
-        if (obj.isNull("current_user")) me = null;
-        else me = new Leader(obj.getJSONObject("current_user"));
+        if (obj.isNull("current_user")) {
+            me = null;
+        } else {
+            Leader me = null;
+            try {
+                me = new Leader(obj.getJSONObject("current_user"));
+            } catch (JSONException ex) {
+                Log.w(TAG, "Failed parsing current_user leader!", ex);
+            }
+
+            this.me = me;
+        }
     }
 }
